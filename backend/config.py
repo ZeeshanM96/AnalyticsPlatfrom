bf0d3289e62379a5853146096ed3f51c0bfa4698
ZeeshanM96 <zeeshanmajeedabdul@gmail.com>
2025-06-14 01:55:57 +0200

    Added Schema File

--- backend/config.py
+++ backend/config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Ideally load environment variables from a .env file
-JWT_SECRET = "MyTestKey"  # Replace with a secure key
+JWT_SECRET = ""  # Replace with a secure key
 JWT_ALGORITHM = "HS256"
 JWT_EXPIRATION_MINUTES = 60
