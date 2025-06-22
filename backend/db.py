54022b14a24fdbbd60fd51364ef4b96725cb93cf
ZeeshanM96 <zeeshanmajeedabdul@gmail.com>
2025-06-22 03:41:20 +0200

    Dockerized the project and created CD pipeline

--- backend/db.py
+++ backend/db.py
@@ -1,16 +1,16 @@
-# Backend/db.py
+# backend/db.py
 import os
 import pyodbc
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def get_connection():
     conn = pyodbc.connect(
         f"DRIVER={{ODBC Driver 17 for SQL Server}};"
         f"SERVER={os.getenv('DB_SERVER')};"
         f"DATABASE={os.getenv('DB_DATABASE')};"
-        "Trusted_Connection=yes;"
+        f"UID={os.getenv('DB_USER')};"
+        f"PWD={os.getenv('DB_PASSWORD')};"
     )
     return conn
