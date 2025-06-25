75b66900239b1bbc811f1a29e4eae06ed988cbc3
Zeeshan Majeed <116648836+ZeeshanM96@users.noreply.github.com>
2025-06-26 01:43:50 +0200

    Removed config as config moved to .env (#11)

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-# # Backend/config.py
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# JWT_SECRET = os.getenv("JWT_SECRET")
-# JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
-# JWT_EXPIRATION_MINUTES = int(os.getenv("JWT_EXPIRATION_MINUTES", 60))
