4634de18f7496f1568aefd95757309cb9235a803
ZeeshanM96 <zeeshanmajeedabdul@gmail.com>
2025-07-01 14:47:02 +0200

    pytest fail fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,16 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from cryptography.fernet import Fernet
 import pytest
 from fastapi.testclient import TestClient
 from backend.main import app
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=".env.local")
+
+if not os.getenv("FERNET_KEY"):
+    os.environ["FERNET_KEY"] = Fernet.generate_key().decode()
 
 @pytest.fixture
 def client():
